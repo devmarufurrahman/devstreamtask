270aa18eaa50d2db88af632226e017683684da75
devmarufurrahman <mdmarufur5682@gmail.com>
2024-10-31 16:47:43 +0600

    bug fixing

--- app/src/main/java/com/maruf/devstream/ui/activity/MainActivity.kt
+++ app/src/main/java/com/maruf/devstream/ui/activity/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(binding.fragmentContainer.id, fragment)
+            .addToBackStack(null)
             .commit()
     }
 }
--- app/src/main/java/com/maruf/devstream/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/maruf/devstream/ui/fragment/HomeFragment.kt
@@ -26,4 +26,5 @@ class HomeFragment : Fragment() {
     }
 
 
+
 }
